31f7b38140de1c839a313d081ad8bd7bb4c82a57
7Yura <svido777@gmail.com>
2022-11-22 01:18:27 +0200

    node hw2

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 const express = require('express');
-require('dotenv').config();
+const fs = require('fs/promises');
 
-const userRouter = require('./router/user.router');
-const configs = require('./config/config');
+const userDB= require('../june-2022-resolve/dataBase/users.json')
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.use('/users', userRouter);
 
-app.get('/', (req, res) => {
-    res.json('WELOCME')
+
+app.get('/users',(req, res) => {
+    res.json(userDB);
+})
+
+app.post('/users',async (req, res) => {
+    const {name,age}= req.body
+    // console.log(userInfo);
+    const newUser = { id: userDB[userDB.length -1].id + 1, name, age }
+    userDB.push(newUser);
+    await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(userDB));
+    res.json(userDB);
+})
+
+app.put('/users/:userid',async (req, res) => {
+    const newUser = req.body;
+    const {userid} = req.params;
+    const index = userDB.findIndex((user) => user.id === +userid);
+
+    if (index === -1) {
+        return res.status(404).json(`User with id ${userid} not found`);
+    }
+    // userDB[index] = newUser;
+    userDB[index] = { ...userDB[index], ...newUser };
+    await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(userDB));
+    res.json(userDB);
+})
+
+app.delete('/users/:userId', async (req, res) => {
+    const { userId } = req.params;
+    // const users = await fileServices.reader();
+    const index = userDB.findIndex((user) => user.id === +userId);
+
+    console.log(index);
+    if (index === -1) {
+        return res.status(404).json(`User with id ${userId} not found`);
+    }
+        userDB.splice(index, 1);
+        await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(userDB));
+        // res.sendStatus(204);
+    res.json(userDB);
 });
 
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        message: err.message || 'Unknown error',
-        status: err.status || 500
-    });
-});
+app.get('/users/:userid',(req, res) => {
+    console.log('users endpoint');
+    // res.json({name: 'Yura'});
+    const{userid}=req.params
+    res.json(userDB[userid]);
+})
 
-app.listen(configs.PORT, () => {
-    console.log(`Server listen ${configs.PORT}`);
-});
+app.listen(5000,()=>{
+    console.log('Server listen 5000');
+})
--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    DB_PASSWORD: process.env.DB_PASSWORD || '5678876'
-}
