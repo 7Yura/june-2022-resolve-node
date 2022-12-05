155eb6b3c1023ce8c36b5e0320c03e73a844e9a5
7Yura <svido777@gmail.com>
2022-12-05 23:33:15 +0200

    forgot_pass

--- service/email.service.js
+++ service/email.service.js
@@ -1,52 +1,81 @@
-const nodemailer = require('nodemailer');
-const EmailTemplates = require('email-templates');
-const path = require('path');
-const { NO_REPLY_EMAIL, NO_REPLY_EMAIL_PASSWORD } = require('../config/config');
-const emailTemplates = require('../email-templates');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const ApiError = require("../error/ApiError");
-
-const sendEmail = async (receiverMail, emailAction, locals = {}) => { // WELCOME: "welcome",
-    console.log(emailAction, '- EMAIL SERVICE')
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: NO_REPLY_EMAIL,
-            pass: NO_REPLY_EMAIL_PASSWORD
-        }
-    });
-
-    console.log('EM TEM -__________________-');
-    console.log(emailTemplates);
-    console.log('EM TEM -__________________-');
-
-    const templateInfo = emailTemplates[emailAction];
-
-    console.log('TEMP INFO (*************************8');
-    console.log(templateInfo);
-    console.log('TEMP INFO (*************************8');
-
-    if (!templateInfo) {
-        throw new ApiError('Wrong template', 500);
-    }
-
-    const templateRenderer = new EmailTemplates({
-        views: {
-            root: path.join(process.cwd(), 'email-templates')
-        }
-    });
-
-    Object.assign(locals || {}, { frontendURL: 'google.com' });
-
-    const html = await templateRenderer.render(templateInfo.templateName, locals);
-
-    return transporter.sendMail({
-        from: 'No reply',
-        to: receiverMail,
-        subject: templateInfo.subject,
-        html
-    });
-}
+const {
+    ACCESS_SECRET,
+    REFRESH_SECRET,
+    CONFIRM_ACCOUNT_ACTION_TOKEN_SECRET,
+    FORGOT_PASSWORD_ACTION_TOKEN_SECRET
+} = require('../config/config');
+const { tokenTypeEnum } = require('../enum');
+const tokenTypes = require("../config/token-action.enum");
 
 module.exports = {
-    sendEmail
+    hashPassword: (password) => bcrypt.hash(password, 10),
+
+    comparePasswords: async (hashPassword, password) => {
+        const isPasswordsSame = await bcrypt.compare(password, hashPassword);
+
+        if (!isPasswordsSame) {
+            throw new ApiError('Wrong email or password', 400);
+        }
+    },
+
+    generateAccessTokenPair: (dataToSign = {}) => {
+        const accessToken = jwt.sign(dataToSign, ACCESS_SECRET, { expiresIn: '15s' });
+        const refreshToken = jwt.sign(dataToSign, REFRESH_SECRET, { expiresIn: '30d' });
+
+        return {
+            accessToken,
+            refreshToken
+        }
+    },
+
+    generateActionToken: (actionType, dataToSign = {}) => {
+        let secretWord = '';
+
+        switch (actionType) {
+            case tokenTypes.CONFIRM_ACCOUNT:
+                secretWord = CONFIRM_ACCOUNT_ACTION_TOKEN_SECRET;
+                break;
+            case tokenTypes.FORGOT_PASSWORD:
+                secretWord = FORGOT_PASSWORD_ACTION_TOKEN_SECRET;
+                break;
+        }
+
+        return jwt.sign(dataToSign, secretWord, { expiresIn: '7d' });
+    },
+
+    checkToken: (token = '', tokenType = tokenTypeEnum.accessToken) => {
+        try {
+            let secret = '';
+
+            if (tokenType === tokenTypeEnum.accessToken) secret = ACCESS_SECRET;
+            else if (tokenType === tokenTypeEnum.refreshToken) secret = REFRESH_SECRET;
+
+            return jwt.verify(token, secret);
+        } catch (e) {
+            throw new ApiError('Token not valid', 401);
+        }
+    },
+
+    checkActionToken: (token, actionType) => {
+        try {
+            let secretWord = '';
+
+            switch (actionType) {
+                case tokenTypes.CONFIRM_ACCOUNT:
+                    secretWord = CONFIRM_ACCOUNT_ACTION_TOKEN_SECRET;
+                    break;
+                case tokenTypes.FORGOT_PASSWORD:
+                    secretWord = FORGOT_PASSWORD_ACTION_TOKEN_SECRET;
+                    break;
+            }
+
+            jwt.verify(token, secretWord);
+        } catch (e) {
+            throw new ApiError('Token not valid', 401);
+        }
+    }
 }
